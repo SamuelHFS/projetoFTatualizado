e2d1578c4b49ad8d342a04a891972d0034fc9a44
SamuelHFS <samuel.jpg1@gmail.com>
2021-09-05 15:18:01 -0300

    Teste dia 05/09/2021 15:17

--- Projeto FT/js/index.js
+++ Projeto FT/js/index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Sep 12, 2021 05:30:25").getTime();
+var countDownDate = new Date("Sep 12, 2021 17:00:00").getTime();
 var x = setInterval(function() {
 
   var now = new Date().getTime();
