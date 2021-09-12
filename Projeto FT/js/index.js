9b223fe3cf10d628607144abf950483971d8c50c
SamuelHFS <samuel.jpg1@gmail.com>
2021-09-12 17:21:08 -0300

    ALTERAÇÃO JS

--- Projeto FT/js/index.js
+++ Projeto FT/js/index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Sep 12, 2021 17:00:00").getTime();
+var countDownDate = new Date("Sep 13, 2021 06:00:00").getTime();
 var x = setInterval(function() {
 
   var now = new Date().getTime();
